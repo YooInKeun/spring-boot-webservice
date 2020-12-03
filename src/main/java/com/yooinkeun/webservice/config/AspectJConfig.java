01ddf07664a120f204f4a674244fe41a3062a36a
YooInKeun <keun0390@naver.com>
2020-12-03 23:34:10 +0900

    deps: add jpa dependency and configuration

---
+++ src/main/java/com/yooinkeun/webservice/config/AspectJConfig.java
@@ -0,0 +1,9 @@
+package com.yooinkeun.webservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectJConfig {
+}

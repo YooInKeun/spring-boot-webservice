081ed8a0b3d6185ed079d322e614d82fd88c1e04
Yoo In Keun <keun0390@naver.com>
2020-12-10 23:00:24 +0900

    feat: add test interceptor

--- src/main/java/com/yooinkeun/webservice/interceptor/TestInterceptor.java
+++ src/main/java/com/yooinkeun/webservice/interceptor/TestInterceptor.java
@@ -1,6 +1,7 @@
 package com.yooinkeun.webservice.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,10 +10,19 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class TestInterceptor extends HandlerInterceptorAdapter {
 
-    // 처리를 하기 전 실행
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
         log.info("prehandle 실행");
         return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle 실행");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object object, Exception arg3) throws Exception {
+        log.info("afterCompletion 실행");
     }
 }

dd31bd21a2b9dbdb37b14c5c6d8147dd79a98768
Yoo In Keun <keun0390@naver.com>
2020-12-10 23:33:33 +0900

    refactor: add interceptor word in log

--- src/main/java/com/yooinkeun/webservice/interceptor/TestInterceptor.java
+++ src/main/java/com/yooinkeun/webservice/interceptor/TestInterceptor.java
@@ -12,17 +12,17 @@ public class TestInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
-        log.info("prehandle 실행");
+        log.info("Interceptor > prehandle 실행");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        log.info("postHandle 실행");
+        log.info("Interceptor > postHandle 실행");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object object, Exception arg3) throws Exception {
-        log.info("afterCompletion 실행");
+        log.info("Interceptor > afterCompletion 실행");
     }
 }

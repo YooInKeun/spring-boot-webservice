e2d830b00173076b7cd974d42ff4401764759a8b
Yoo In Keun <keun0390@naver.com>
2020-12-22 23:56:21 +0900

    feat: add team entity

---
+++ src/main/java/com/yooinkeun/webservice/domain/team/Team.java
@@ -0,0 +1,21 @@
+package com.yooinkeun.webservice.domain.Team;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+
+    @Id
+    @Column(name = "TEAM_ID")
+    private String id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+}

de7a7a927aea154fd0b6032f7dd3e4c9c6fd1902
Yoo In Keun <keun0390@naver.com>
2020-12-22 23:58:54 +0900

    feat: add MemberRepository

---
+++ src/main/java/com/yooinkeun/webservice/domain/team/MemberRepository.java
@@ -0,0 +1,6 @@
+package com.yooinkeun.webservice.domain.Team;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
